2c25d0b22e740473f48b4eebbf7204ef820c6881
mavensingh <kuldeep.avsar@gmail.com>
2020-05-23 19:38:39 +0530

    added ticker to run the tool in every 5 minutes and check the battery percentage

--- main.go
+++ main.go
@@ -21,34 +21,53 @@ func BatteryInforamtion() {
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
-	fmt.Println("Command Successfully Executed")
+
 	output := string(out[:])
 	per := strings.Replace(output, "\n", ",", -1)
 	per = strings.Replace(per, " ", "", -1)
 	per = strings.TrimRight(per, ",")
 	values := strings.Split(per, ",")
 
-	// hoursLeft := values[11][12:]
+	hoursLeft := values[11][12:]
 	BatteryPercentage := values[12][11:13]
-	// fmt.Println(values)
-	fmt.Println(values[12][11:])
-	fmt.Println(BatteryPercentage)
-	if BatteryPercentage <= "23" {
-		command := "espeak"
-		voice := "-v"
-		langauge := "en-us"
-		gender := "+f3"
-		voices := voice + langauge + gender
-		speed := "-s130"
-		message := fmt.Sprintf("Kuldeep, Battery is Low. Level %s", BatteryPercentage)
-		cmd := exec.Command(command, voices, speed, message)
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-
-		}
-		StatusFile()
+	ChargingStatus := values[6][6:]
+	fmt.Println()
+	fmt.Println("Remaining : ", hoursLeft)
+	fmt.Println("Percentage : ", values[12][11:])
+	fmt.Println()
+	_, err = os.Stat("status.txt")
+	if err != nil {
+		CheckBattery(BatteryPercentage, ChargingStatus)
+	} else {
+		return
 	}
+
+}
+func CheckBattery(BatteryPercentage, ChargingStatus string) {
+	if BatteryPercentage <= "23" && ChargingStatus == "discharging" {
+		message := fmt.Sprintf("Please connect the charger")
+		Alert(message)
+	} else if BatteryPercentage == "99" && ChargingStatus == "charging" {
+		message := fmt.Sprintf("Charge full, Please Remove the charger")
+		Alert(message)
+	}
+}
+
+func Alert(message string) {
+	command := "espeak"
+	voice := "-v"
+	langauge := "en-us"
+	gender := "+f3"
+	voices := voice + langauge + gender
+	speed := "-s130"
+	cmd := exec.Command(command, voices, speed, message)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	StatusFile()
+
 }
 
 func StatusFile() {
@@ -109,21 +128,16 @@ func main() {
 	if runtime.GOOS == "windows" {
 		fmt.Println("Can't Execute this on a windows machine")
 	} else {
-		fmt.Println(runtime.GOOS)
+		// fmt.Println(runtime.GOOS)
 		// Battery Percentage Alert Message
-		ticker := time.NewTicker(1 * time.Minute)
+		ticker := time.NewTicker(5 * time.Minute)
 		quit := make(chan struct{})
 		wg.Add(1)
 		go func() {
 			for {
 				select {
 				case <-ticker.C:
-					_, err := os.Stat("status.txt")
-					if err == nil {
-						fmt.Printf("File exists\n")
-					} else {
-						BatteryInforamtion()
-					}
+					BatteryInforamtion()
 				case <-quit:
 					ticker.Stop()
 					return
@@ -131,6 +145,5 @@ func main() {
 			}
 		}()
 		wg.Wait()
-		// Speak()
 	}
 }
